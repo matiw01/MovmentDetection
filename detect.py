3f5edec04a6ce68fabb8be1576787ea0cd687375
Luki <hexeyxd@gmail.com>
2022-05-09 20:37:52 +0200

    debug mode added.

--- detect.py
+++ detect.py
@@ -2,17 +2,40 @@ import cv2 as cv
 from get_background import get_background
 
 path = "http://live.uci.agh.edu.pl/video/stream1.cgi?start=1543408695"
-debug =True
+debug = True
+
+import PySimpleGUI as sg
+
+layout = [[sg.Text(" mask coordinates can be hardcoded in detect.py line - 53: (top left corner is 0,0) ")],[sg.Text("enter your source:"),
+           sg.InputText(default_text="http://live.uci.agh.edu.pl/video/stream1.cgi?start=1543408695")],
+          [sg.Button("Debug"), sg.Button("Normal Run")]
+          #[sg.Text("enter mask coordinates: (top left corner is 0,0):")]
+          ]
+
+window = sg.Window(title="Motion detection", layout=layout, margins=(100, 50))
+while True:
+    event, values = window.read()
+    path =values[0]
+    # End program if user closes window or
+    # presses the OK button
+    if event == "Debug":
+        debug = True
+        break
+    if event == "Normal Run":
+        debug = False
+        break
+window.close()
 cap = cv.VideoCapture(path)
-print("captured")
 
 if not cap.isOpened():
-    print("Cannot open camera")
+    print("Cannot open camera ", path)
     exit(1)
+else:
+    print("opened camera at",path)
 
 background = get_background(cap)
-if (debug==True):
-    cv.imshow("background",background);
+if (debug == True):
+    cv.imshow("background", background)
 print("got median")
 
 frame_count = 0
@@ -26,8 +49,11 @@ frame_height = int(cap.get(4))
 #     cv.VideoWriter_fourcc(*'mp4v'), 10,
 #     (frame_width, frame_height)
 # )
-mask = (0, 0, 5000, 5000)
 
+mask = (0, 0, frame_width, int(frame_height / 2))
+
+
+# print(mask[0])
 
 def in_mask(x1, y1, x2, y2, mask):
     a, b, c, d = mask
@@ -41,26 +67,26 @@ while (cap.isOpened()):
         orig_frame = frame.copy()
         # IMPORTANT STEP: convert the frame to grayscale first
         gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
-        if(debug==True):
+        if (debug == True):
             cv.imshow('phase1', gray)
         if frame_count % consecutive_frame == 0 or frame_count == 1:
             frame_diff_list = []
         # find the difference between current frame and base frame
         frame_diff = cv.absdiff(gray, background)
-        if(debug==True):
+        if (debug == True):
             cv.imshow('phase2', frame_diff);
         # thresholding to convert the frame to binary
         ret, thres = cv.threshold(frame_diff, 50, 255, cv.THRESH_BINARY)
-        if(debug==True):
+        if (debug == True):
             cv.imshow('phase3', thres);
         # dilate the frame a bit to get some more white area...
         # ... makes the detection of contours a bit easier
-        dilate_frame = cv.dilate(thres, None, iterations=3)
-        if(debug==True):
-            cv.imshow('phase4', thres);
+        dilate_frame = cv.dilate(thres, None, iterations=2)
+        if (debug == True):
+            cv.imshow('phase4', dilate_frame);
 
         # append the final result into the `frame_diff_list`
-        frame_diff_list.append(thres)
+        frame_diff_list.append(dilate_frame)
         # if we have reached `consecutive_frame` number of frames
         if len(frame_diff_list) == consecutive_frame:
             # add all the frames in the `frame_diff_list`
@@ -78,9 +104,12 @@ while (cap.isOpened()):
                 # get the xmin, ymin, width, and height coordinates from the contours
                 (x, y, w, h) = cv.boundingRect(contour)
                 # draw the bounding boxes
-                if not in_mask(x, y, x+w, y+h, mask):
+                if not in_mask(x, y, x + w, y + h, mask):
                     cv.rectangle(orig_frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
-
+            #print(mask)
+            #print(mask[0])
+            cv.rectangle(orig_frame, (mask[0], mask[1]), (mask[2], mask[3]), (255, 0, 0), 3)
+            # cv.rectangle(orig_farmask)
             cv.imshow('Detected Objects', orig_frame)
 
             # out.write(orig_frame)
